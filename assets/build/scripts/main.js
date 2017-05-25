0fb9718088b799f57362ebd32dccb89f23bacd22
Paul Marshall <paul@teamfc.co.uk>
2017-05-26 00:14:59 +0100

    Initial commit

---
+++ assets/build/scripts/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+
+  $('.slick-slider').slick({
+    fade: true,
+    speed: 100,
+  });
+
+});
---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Slick Slider</title>
+<link href="/assets/build/styles/main.min.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <header class="banner">
+    <div class="row row--full middle-xs center-xs">
+      <div class="col-xs-12">
+        <div class="container">
+          <div class="white--background">
+            <h1 class="rob-bold beta beta--leading tracking--n30 primary">Slick Slider</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <main>
+    <section class="section">
+      <div class="container">
+
+        <div class="slick-slider">
+          <div class="slick-slider__item row row--full">
+            <div class="slick-slider__content col-xs-6"><img src="/assets/images/image-1.jpg"></div>
+            <div class="slick-slider__img col-xs-6"><img src="/assets/images/image-2.jpg"></div>
+          </div>
+          <div class="slick-slider__item row row--full">
+            <div class="slick-slider__content col-xs-6"><img src="/assets/images/image-3.jpg"></div>
+            <div class="slick-slider__img col-xs-6"><img src="/assets/images/image-4.jpg"></div>
+          </div>
+        </div>
+
+      </div>
+    </section>
+  </main>
+
+  <script src="/assets/vendor/jquery-3.2.1.min.js"></script>
+  <script src="/assets/vendor/slick.min.js"></script>
+  <script src="/assets/build/scripts/main.min.js"></script>
+
+</body>
+
+</html>
